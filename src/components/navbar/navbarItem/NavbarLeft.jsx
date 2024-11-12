39076f93ac2c3b4ba8f98467ba74a6b700d05096
funwithpeeves <sedefyildiz3845@gmail.com>
2024-11-12 13:31:48 +0300

    first commit

---
+++ src/components/navbar/navbarItem/NavbarLeft.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const NavbarLeft = () => {
+  const navigate = useNavigate();
+
+  return <div onClick={() => {navigate("/")}} className='text-6xl cursor-pointer'>Alışveriş Dünyası</div>
+}
+
+export default NavbarLeft
